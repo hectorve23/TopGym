fa3061d9d70591712b0af3d151054622feb1fe35
DAM2Alu15 <DAM2Alu15@B01-PC11>
2026-02-23 13:29:17 +0100

    holf

---
+++ TopGym/VentanaUsuario.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TopGym
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaUsuario.xaml
+    /// </summary>
+    public partial class VentanaUsuario : Window
+    {
+        public VentanaUsuario()
+        {
+            InitializeComponent();
+        }
+    }
+}
